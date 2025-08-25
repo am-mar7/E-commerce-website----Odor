af9b1fc35cdb85fb211341e608c122ff32d8b540
Ammar <ammaralaa470@gmail.com>
2025-08-25 22:53:12 +0300

    finshed home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,38 @@
 fetch("../components/header.html")
   .then(response => response.text())
   .then(data => {
-    document.getElementById("header").innerHTML = data;
-      
+    document.querySelector(".fetch-header").innerHTML = data
+
+    // any js code  related to header will be written here 
+  const navParts = document.querySelectorAll('.navpart')
+  const navtext =  document.querySelectorAll('.nav-item > *')
+  const navbar = document.querySelector('.navbar-expand-md')
+  const cartIcon = document.querySelector('.nav-item svg')
+  window.addEventListener('scroll' , ()=>{
+    if(window.scrollY > 80){
+      navParts[0].classList.add('hidden')
+      navParts[1].classList.add('bg-blur')
+      navParts[1].classList.add('fixed-top')
+      navbar.classList.add('navbar-dark')
+      navtext.forEach((text)=>{
+        text.classList.add('text-light')
+      })
+      cartIcon.style.fill = '#f8f8f8'
+      navParts[1].classList.remove('border-bottom')
+    }
+    else{
+      navParts[0].classList.remove('hidden')
+      navbar.classList.remove('navbar-dark')
+      navParts[1].classList.remove('fixed-top')
+      navParts[1].classList.remove('bg-blur')
+      navtext.forEach((text)=>{
+        text.classList.remove('text-light')
+      })
+      navParts[1].classList.add('border-bottom')
+      cartIcon.style.fill = 'orangered'
+    }
+  })
+
   const links = document.querySelectorAll('.nav-link') 
   path = window.location.pathname.split('/').pop()
   links.forEach((link)=>{
@@ -13,14 +43,52 @@ fetch("../components/header.html")
   })
   .catch(error => console.error("Error loading header:", error));
 
+fetch("../components/footer.html").then(response => response.text()).then(data =>{
+  document.querySelector('footer').innerHTML = data
+})
+  
 
 const heroContent = document.querySelectorAll('.hero-content > *')
-console.log(heroContent)
 heroContent.forEach((hc, idx) => {
   setTimeout(() =>{
       hc.classList.add('appear')
-  }, idx * 900);
+  },2500 +  idx * 800);
+})
+
+let swiper = new Swiper('.swiper', {
+  direction: 'horizontal',    
+  slidesPerGroup: 1, 
+  loop: true,
+  autoplay: {
+    delay: 3500,      
+    disableOnInteraction: false,
+  },
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    600:{
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    900:{
+      slidesPerView: 3,     
+      spaceBetween: 30,
+    },
+    1400:{
+      slidesPerView: 4,     
+      spaceBetween: 30,
+    }
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
 });
+
 
 
 
